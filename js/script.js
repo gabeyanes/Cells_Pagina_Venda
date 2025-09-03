7ba8f8437c64b1f3bb97663248c712755213ba54
Gabriel Yanes <gabriel.yanes1217@outlook.com>
2025-09-03 12:00:33 -0300

    1° Commit
    Deploy Teste da página

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    grabCursos: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        300: {
+            slidesPerView: 1,
+            spaceBetween: 12
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 1,
+            spaceBetween: 16
+        },
+        // when window width is >= 640px
+        1000: {
+            slidesPerView: 3,
+            spaceBetween: 32
+        }
+    }
+});
+
+new Accordion([".accordion-container", ".accordion-container2"], {})
+
